7746f221c834d65c8223f4401a250dc7becc5d6e
Athena Boose <pestpestthechicken@yahoo.com>
2024-09-30 21:45:33 -0700

    Added finite state machine to store any data about tag attributes since I couldn't think of a nice way to do it with the regexes implemented in Rust's Regex crate.
    There are also a bunch of other changes and the code isn't quite fully
    implemented yet but it's getting there.

--- src/token_loader/xml_parser.rs
+++ src/token_loader/xml_parser.rs
@@ -1,50 +1,185 @@
-use std::collections::HashMap;
+use std::{error::Error, fmt, collections::HashMap, sync::LazyLock};
 use regex::Regex;
 
-pub struct Node {
-    children: Vec<Node>,
-    node_type: String,
-    node_data: Option<String>,
-    attributes: HashMap<String, String>,
-}
+#[derive(Debug)]
+pub struct XMLParsingError;
 
-impl Node {
-    pub fn new() -> Node {
-        return Node {
-            children: Vec::new(),
-            node_type: String::with_capacity(16),
-            node_data: None,
-            attributes: HashMap::new(),
-        };
+impl Error for XMLParsingError {}
+
+impl fmt::Display for XMLParsingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", "The XML could not be parsed. Maybe you forgot an ending tag?")
     }
 }
 
+/// A node of the DOM for the parsed XML
+pub struct Node {
+    pub children: Vec<Node>,
+    pub node_type: Option<String>,
+    pub attributes: HashMap<String, String>,
+}
+
+// global variables aren't great but they were neater than passing the same regexes everywhere or compiling them repeatedly
+static TAG: LazyLock<Regex> = LazyLock::new(|| Regex::new(r#"\A<([a-zA-Z_:][a-zA-Z_:\-.0-9]*)(?: [a-zA-Z_:][a-zA-Z_:\-.0-9]*=['"].+?['"])*>"#).unwrap());
+static DATA: LazyLock<Regex> = LazyLock::new(|| Regex::new(r#"\A(.*?)</([a-zA-Z_:][a-zA-Z_:\-.0-9]*)>"#).unwrap());
+static WHITESPACE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\A\s*").unwrap());
+
+
+enum State {
+    Key,
+    Value,
+}
+
+impl Node {
+    /// Creates a new Node with `nodetype` as its type.
+    /// This type of node does not have data, but it can 
+    pub fn new_structure_node(nodetype: String) -> Node {
+        return Node {
+            children: Vec::new(),
+            node_type: Some(nodetype),
+            attributes: HashMap::new(),
+        }
+    }
+    pub fn new_data_node(data: String) -> Node {
+        return Node {
+            children: Vec::new(),
+            node_type: None,
+            attributes: HashMap::new(),
+        }
+    }
+    
+    /// Sets any attributes of the node given the tag associated with it
+    /// Pass the entire matched tag into this function, <brackets> and all.
+    fn set_attributes(&mut self, tag: String) {
+
+        let mut key: String = String::new();
+        let mut val: String = String::new();
+        let mut tag_iter = tag.chars().peekable();
+
+        match tag.chars().position(|current_char| current_char == ' ') {
+            Some(start_position) => {
+                tag_iter.nth(start_position);
+            },
+            None => {
+                return;
+            },
+        }
+        let mut state = State::Key;
+        
+        // this much nesting hurts my soul
+        while let Some(current_char) = tag_iter.next() {
+            match state {
+                State::Key => {
+                    match current_char {
+                        ' ' => {}, // dont really care about spaces
+                        '=' => {
+                            state = State::Value;
+                            tag_iter.next(); // advancing the iterator so that the value state doesn't have to deal with opening/closing quotes
+                        }
+                        _ => key.push(current_char),
+                    }
+                }
+                State::Value => {
+                    match current_char {
+                        '>' => {
+                            self.attributes.insert(key.clone(), val.clone());
+                            return;
+                        }
+                        '"' | '\'' => {
+                            self.attributes.insert(key.clone(), val.clone());
+                            key.clear();
+                            val.clear();
+                            state = State::Key
+                        }
+                        _ => val.push(current_char),
+                    }
+                }
+            }
+        }
+    }
+}
+
+/// A function to parse an XML file into a DOM.
+/// 
+/// This isn't a general XML parser, it's only intended to parse the tokensheet specifically.
+/// It could probably be turned into a more general, proper XML parser if I cared and had the time, but this works.
 pub fn parse_xml(mut text: &str) {
-    // quickly matching the version and comment from the xml since we don't care about those
+
+    // quickly matching the version and comment from the XML since we don't care about those
     // if an issue occurs here, we don't care, but we should tell the user that they're being an idiot.
     match xml_version(text) {
         Some(txt) => text = txt,
-        None => println!("Warning: No XML version specifier provided in tokensheet."),
+        None => println!("Warning: Either no XML version specifier was provided in tokensheet or the specifier was incorrectly formatted."),
     }
     match comment(text) {
         Some(txt) => text = txt,
         None => {}, // idk if this is how I am supposed to make None not do anything but it should work
     }
+    let mut main_node = Node::new_structure_node("main".to_string());
+
+    make_node(Ok(text), &mut main_node);
 
 }
 
 
+fn make_node<'a>(file_slice: Result<&'a str, XMLParsingError>, last_node: &mut Node) -> Result<Option<&'a str>, XMLParsingError>{
+    // passing in references to all the regexes is cursed but I couldn't think of a better way
+    let end_tag: String;
+    let mut slice: &str; // slice itself is mutable, but the reference it holds isn't. so cursed. 
+    slice = file_slice?;
+    
+    slice = grab_whitespace(slice);
+    match TAG.captures(slice) {
+        Some(result) => {
+            let mut current_node: Node = Node::new_structure_node(result.get(1).unwrap().as_str().to_string()); // we can unwrap because this will always be in the match
+            end_tag = format!("</{}>", current_node.node_type.unwrap()); // unwrap is safe here, we just set the node type so we know it is not None
+            slice = &slice[result.get(0).unwrap().len()..]; // advancing slice
 
-fn xml_version(file_slice: &str) -> Option<&str> {
-    let version: Regex = Regex::new(r#"\A\w*<\?xml version="[0-9]\.[0-9]" encoding="UTF-8"\?>\n?"#).unwrap();
+            // grabbing all of the subtags we can. make_node will put those subtags into current_node.children so we don't have to worry about anything
+            loop {
+                match make_node(Ok(slice), &mut current_node)? { // if an error's been thrown we also want to throw that error
+                    Some(new_slice) => slice = new_slice,
+                    None => break,
+                }
+            }
+            // if the next tag isn't our end tag, the XML is not formatted correctly
+            if slice[..end_tag.len()] != end_tag {
+                return Err(XMLParsingError);
+            } else {
+                slice = &slice[end_tag.len()..];
+            }
+
+        }
+        None => {
+            let valid_next_ending_tag = format!("</{}>", last_node.node_type.expect("Error: Data nodes should not recursively call make_node.")).as_str();
+            if &slice[..valid_next_ending_tag.len()] == valid_next_ending_tag {
+                return Err(Box::new(NoNextTag));
+            }
+        }
+    }
+    println!("ooga booga");
+}
+
+fn grab_whitespace<'a>(file_slice: &'a str) -> &'a str {
+    match WHITESPACE.find(file_slice) {
+        Some(result) => return &file_slice[result.end()..],
+        None => file_slice,
+    }
+}
+
+
+fn xml_version<'a>(mut file_slice: &'a str) -> Option<&'a str> {
+    let version: Regex = Regex::new(r#"\A<\?xml version="[0-9]\.[0-9]" encoding="UTF-8"\?>\n?"#).unwrap();
+    file_slice = grab_whitespace(file_slice);
     match version.find(file_slice) {
         Some(result) => return Some(&file_slice[result.end()..]),
         None => return None,
     };
 }
 
-fn comment(file_slice: &str) -> Option<&str> {
-    let comment: Regex = Regex::new(r"\A\w*<!--(?:.|\n|\r\n)*-->?").unwrap();
+fn comment<'a>(mut file_slice: &'a str) -> Option<&'a str> {
+    let comment: Regex = Regex::new(r"\A<!--(?:.|\n|\r\n)*-->?").unwrap();
+    file_slice = grab_whitespace(file_slice);
     match comment.find(file_slice) {
         Some(result) => return Some(&file_slice[result.end()..]),
         None => return None,
